065c29f106915aa6d630ef0d932b120049dce85c
Syifa Kaffa Billah <syifa.kaffa@ui.ac.id>
2023-09-20 15:34:20 +0700

    selesai tutorial 3

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from main.views import show_main, create_product, show_xml, show_json
+from main.views import register, login_user, logout_user #sesuaikan dengan nama fungsi yang dibuat
 
 app_name = 'main'
 
@@ -8,4 +9,7 @@ urlpatterns = [
     path('create-product', create_product, name='create_product'),
     path('xml/', show_xml, name='show_xml'), 
     path('json/', show_json, name='show_json'), 
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
